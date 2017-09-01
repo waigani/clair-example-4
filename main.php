9afad4a4d15c6859d0608cc6a19fd93651804715
emersonwood <emersonwood94@gmail.com>
2017-09-01 15:15:24 +1200

    Update

--- main.php
+++ main.php
@@ -1,13 +1,19 @@
 <?php
 
 function ExecuteSQL ($query) {
-	return "SQL result\n";
+	return sprintf("Some result from \"%s\"\n\n", $query);
 }
 
 function Main() {
-	$userNamesSQL = "SELECT name FROM Users";
-	$userNames = ExecuteSQL($userNamesSQL);
+	$getUserNamesSQL = "SELECT UserName FROM Users;";
+	$userNames = ExecuteSQL($getUserNamesSQL);
 	print $userNames;
+
+	print("Please enter your username: ");
+	$userName = rtrim(fgets(STDIN), "\n");
+	$getUserDataSQL = "SELECT * FROM Users WHERE UserName = " . $userName . ";";
+	$userData = ExecuteSQL($getUserDataSQL);
+	print $userData;
 }
 
 Main();
