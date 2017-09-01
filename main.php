605feee2a47521533e0f1e5d513423019783b9ff
emersonwood <emersonwood94@gmail.com>
2017-09-01 15:10:58 +1200

    Add main.php

---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+
+function ExecuteSQL ($query) {
+	return "SQL result\n";
+}
+
+function Main() {
+	$userNamesSQL = "SELECT name FROM Users";
+	$userNames = ExecuteSQL($userNamesSQL);
+	print $userNames;
+}
+
+Main();
